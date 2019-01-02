3019475c56a345784a83d857020dc24432213f6b
karantin2020 <karantin3030@mail.ru>
2019-01-02 19:20:46 +0500

    fix: Edited tag message
    Edited tag message to include app name
    
    * Tag message

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/karantin2020/gitcomm"
 )
 
-const version = "gitcomm 0.2.2"
+const version = "gitcomm 0.2.3"
 
 func main() {
 	app := cli.App("gitcomm", "Automate git commit messaging")
